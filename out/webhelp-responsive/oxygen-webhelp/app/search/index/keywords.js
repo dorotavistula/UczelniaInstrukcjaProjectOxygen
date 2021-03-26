9adca7ea7f98338b7f898ff17bb85eef3f7ecdfd
dorota <d.cakar@vistula.edu.pl>
2021-03-26 22:16:31 +0100

    25.03

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"WYNIK",p:["p0"]},{w:"REJESTRACJI",p:["p0","p8"]},{w:"ADRES",p:["p1"]},{w:"DO",p:["p1"]},{w:"SYSTEMU",p:["p1"]},{w:"IKR",p:["p1"]},{w:"Akademia",p:["p2"]},{w:"Pedagogiki",p:["p2","p5"]},{w:"Specjalnej",p:["p2","p5"]},{w:"im.",p:["p2"]},{w:"Marii",p:["p2"]},{w:"Grzegorzewskiej",p:["p2"]},{w:"Potrzebne",p:["p3"]},{w:"linki",p:["p3"]},{w:"WYB\u00D3R",p:["p4"]},{w:"KIERUNKU",p:["p4"]},{w:"STUDI\u00D3W",p:["p4"]},{w:"Logo",p:["p5"]},{w:"Akademii",p:["p5"]},{w:"FORMULARZE",p:["p6"]},{w:"OSOBOWE",p:["p6"]},{w:"OP\u0141ATA",p:["p7"]},{w:"REKRUTACYJNA",p:["p7"]},{w:"PROCEDURA",p:["p8"]},{w:"NA",p:["p8"]},{w:"STUDIA",p:["p8"]},{w:"W",p:["p8"]},{w:"SYSTEMIE",p:["p8"]},{w:"IRK",p:["p8"]},{w:"WYPE\u0141NIENIE",p:["p9"]},{w:"ZG\u0141OSZENIA",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4, 5];
+ph["p2"]=[6, 7, 8, 9, 10, 11];
+ph["p3"]=[12, 13];
+ph["p4"]=[14, 15, 16];
+ph["p5"]=[17, 18, 7, 8];
+ph["p6"]=[19, 20];
+ph["p7"]=[21, 22];
+ph["p8"]=[23, 1, 24, 25, 26, 27, 28];
+ph["p9"]=[29, 30];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
