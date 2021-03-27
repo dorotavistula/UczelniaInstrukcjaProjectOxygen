c57d8ec118af446e8554165df6e2b6f734c24e91
dorota <d.cakar@vistula.edu.pl>
2021-03-27 21:45:38 +0100

    27.03

--- out/oxygen-webhelp/app/config.js
+++ out/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210326223332';
+                args = '20210327214256';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
